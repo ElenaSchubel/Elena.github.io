880bf4da8f0dfb1cd8ecac9ddb70c61da23a60df
ElenaSchubel <ebassist@live.com>
2017-03-10 13:16:03 +1300

    fixed javascript and uptdated index

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-//  Created Slide Index Variable and function
-var slideIndex = 1;
-showSlides(slideIndex);
-viewSlides(slideIndex);
+
+var slideIndex = 0;
+
+var t;
+var timedelay=2000;
+
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -11,36 +13,33 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += "active";
-}
 
-var slideIndex = 0;
-showSlides();
-
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
+  function showSlides(n) {
     var i;
-    var slides = document.getElementsByClassName("slides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex> slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 5000); // Change image every 4 seconds
+   var slides = document.getElementsByClassName("slides");
+   var dots = document.getElementsByClassName("dot");
+   if (n==undefined){n = ++slideIndex}
+   if (n > slides.length) {slideIndex = 1}
+   if (n < 1) {slideIndex = slides.length}
+   for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+   }
+   for (i = 0; i < dots.length; i++) {
+       dots[i].className = dots[i].className.replace(" active", "");
+   }
+
+   slides[slideIndex-1].style.display = "block";
+   dots[slideIndex-1].className += " active";
+    t = setTimeout(showSlides, timedelay);
 }
+
+
+function pauseSlides() {
+
+              clearTimeout(t)
+          }
+
+          function startSlides() {
+
+              t = setTimeout(showSlides, timedelay);
+          }
